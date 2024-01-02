13db75eeb90eb631cd4145d259f60aaeddcaa8cc
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-01-02 11:30:04 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/ezeikeaf_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ezeikeaf_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+          ),
+          itemBuilder: (context, index) {
+            return Image.asset('image1.jpg');
+          }),
+    );
+  }
+}
