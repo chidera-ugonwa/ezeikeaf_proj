c9eae195427e8b5f5195f2dcaa2c477e54659a20
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-01-03 13:32:52 +0100

    completed carousel

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,15 +9,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int activePage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GridView.builder(
-      child: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover, image: AssetImage('image1.jpg'))),
-      ),
-    ));
+            itemCount: 3,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1, childAspectRatio: 1.75),
+            itemBuilder: (context, index) {
+              return GridTile(
+                header: const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'ezikeaf',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ),
+                footer: Padding(
+                    padding: const EdgeInsets.all(400),
+                    child: Column(
+                      children: [
+                        const Text('Title'),
+                        const Text('Location'),
+                        ElevatedButton(
+                            onPressed: () {}, child: const Text('Learn More'))
+                      ],
+                    )),
+                child: CarouselSlider.builder(
+                  options: CarouselOptions(
+                    onPageChanged: (val, _) {
+                      setState(() {
+                        activePage = val;
+                      });
+                    },
+                    aspectRatio: 1,
+                    viewportFraction: 1,
+                    enableInfiniteScroll: true,
+                  ),
+                  itemCount: 5,
+                  itemBuilder: (context, itemIndex, realIndex) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('image1.jpg'))),
+                    );
+                  },
+                ),
+              );
+            }));
   }
 }
