877ed1d6d95ad41e481502df320d604b7a2282d8
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-01-04 13:05:37 +0100

    test3

--- lib/home.dart
+++ lib/home.dart
@@ -30,37 +30,30 @@ class _HomeState extends State<Home> {
                         fontSize: 30),
                   ),
                 ),
-                footer: Padding(
-                    padding: const EdgeInsets.all(400),
-                    child: Column(
-                      children: [
-                        const Text('Title'),
-                        const Text('Location'),
-                        ElevatedButton(
-                            onPressed: () {}, child: const Text('Learn More'))
-                      ],
-                    )),
-                child: CarouselSlider.builder(
-                  options: CarouselOptions(
-                    onPageChanged: (val, _) {
-                      setState(() {
-                        activePage = val;
-                      });
+                footer: const Text('Title'),
+                child: Stack(children: [
+                  CarouselSlider.builder(
+                    options: CarouselOptions(
+                      onPageChanged: (val, _) {
+                        setState(() {
+                          activePage = val;
+                        });
+                      },
+                      aspectRatio: 1,
+                      viewportFraction: 1,
+                      enableInfiniteScroll: true,
+                    ),
+                    itemCount: 5,
+                    itemBuilder: (context, itemIndex, realIndex) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('image1.jpg'))),
+                      );
                     },
-                    aspectRatio: 1,
-                    viewportFraction: 1,
-                    enableInfiniteScroll: true,
                   ),
-                  itemCount: 5,
-                  itemBuilder: (context, itemIndex, realIndex) {
-                    return Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage('image1.jpg'))),
-                    );
-                  },
-                ),
+                ]),
               );
             }));
   }
