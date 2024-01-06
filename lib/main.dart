5c5cfd3a5d502e78295f9fe1a4d9b2757bce4081
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-01-06 15:19:19 +0100

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:ezeikeaf_proj/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBy6EaiKmzPKFmfvMpc-O6MMh0Htpp-pxc",
+      appId: "1:475622177334:android:25ad8df5d45012f49f7697",
+      messagingSenderId:
+          "475622177334-sq489js8aq9542t7t6prp7s55jb9lu7j.apps.googleusercontent.com",
+      projectId: "lodgeme-9499e",
+      storageBucket: "lodgeme-9499e.appspot.com",
+    ),
+  );
   runApp(const MyApp());
 }
 
