3ede20be7f952ce325b4c9117f562372745998e1
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-01-12 19:23:34 +0100

    adjusted render

--- lib/home.dart
+++ lib/home.dart
@@ -12,89 +12,94 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int activePage = 0;
 
-  Future getListings() async {
+  Stream<QuerySnapshot> getListings() {
     final db = FirebaseFirestore.instance;
     final projects = db.collection('projects');
-    var check = projects
-        .where('year', isEqualTo: '2023')
-        .where("status", isEqualTo: 'active')
+    return projects
+        // .where('year', isEqualTo: '2023')
+        //.where("status", isEqualTo: 'active')
         .snapshots();
-    Future<bool> checks = check.isEmpty;
-    debugPrint(checks.toString());
-  }
-
-  @override
-  void initState() {
-    getListings();
-    super.initState();
+    //var checks = await check.isEmpty;
+    // debugPrint(checks.toString());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GridView.builder(
-            itemCount: 3,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1, childAspectRatio: 1.75),
-            itemBuilder: (context, index) {
-              return GridTile(
-                header: const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'ezikeaf',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  ),
-                ),
-                footer: const Text('Title'),
-                child: Stack(fit: StackFit.expand, children: [
-                  Positioned(
-                    child: CarouselSlider.builder(
-                      options: CarouselOptions(
-                        onPageChanged: (val, _) {
-                          setState(() {
-                            activePage = val;
-                          });
-                        },
-                        aspectRatio: 1,
-                        viewportFraction: 1,
-                        enableInfiniteScroll: true,
-                      ),
-                      itemCount: 5,
-                      itemBuilder: (context, itemIndex, realIndex) {
-                        return Container(
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage('image1.jpg'))),
-                        );
-                      },
-                    ),
-                  ),
-                  Positioned(
-                      top: 200,
-                      bottom: 200,
-                      right: 200,
-                      left: 200,
-                      child: Center(
-                          child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Text('Title of the Project',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 24)),
-                          const Text('Location of the Project',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18)),
-                          OutlinedButton(
-                              onPressed: () {}, child: const Text('Learn More'))
-                        ],
-                      )))
-                ]),
-              );
+        body: StreamBuilder(
+            stream: getListings(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error ${snapshot.error}');
+              }
+              if (!snapshot.hasData) {
+                return const Center(child: Text('Loading...'));
+              }
+              return GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1, childAspectRatio: 1.75),
+                  children: snapshot.data!.docs
+                      .map((document) => _buildGridItem(document))
+                      .toList());
             }));
   }
+}
+
+Widget _buildGridItem(DocumentSnapshot document) {
+  Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+
+  return GridTile(
+    header: const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Text(
+        'ezikeaf',
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+      ),
+    ),
+    child: Stack(fit: StackFit.expand, children: [
+      Positioned(
+        child: CarouselSlider.builder(
+          options: CarouselOptions(
+            //onPageChanged: (val, _) {
+            //setState(() {
+            //activePage = val;
+            //});
+            //},
+            aspectRatio: 1,
+            viewportFraction: 1,
+            enableInfiniteScroll: true,
+          ),
+          itemCount: 5,
+          itemBuilder: (context, itemIndex, realIndex) {
+            return Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        data['photoLink'],
+                      ))),
+            );
+          },
+        ),
+      ),
+      Positioned(
+          top: 200,
+          bottom: 200,
+          right: 200,
+          left: 200,
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(data['title'],
+                  style: const TextStyle(color: Colors.white, fontSize: 24)),
+              Text(data['location'],
+                  style: const TextStyle(color: Colors.white, fontSize: 18)),
+              OutlinedButton(onPressed: () {}, child: const Text('Learn More'))
+            ],
+          )))
+    ]),
+  );
 }
