498c5b68160ceba5803c0260f7afb3cbb36ba06e
Chidera_Ugonwa <austinugonwa@gmail.com>
2024-02-03 18:36:02 +0100

    filter completed

--- lib/home.dart
+++ lib/home.dart
@@ -30,44 +30,39 @@ class _HomeState extends State<Home> {
       if (projectTypology.isNotEmpty && status.isNotEmpty && year.isNotEmpty) {
         debugPrint('one');
         final db = FirebaseFirestore.instance;
-        final projects = db
-            .collection('projects')
-            .where(
+        final projects = db.collection('projects').where(Filter.and(
+            Filter(
               'project typology',
               whereIn: projectTypology,
-            )
-            .where('status', whereIn: status)
-            .where('year', whereIn: year);
+            ),
+            Filter('status', whereIn: status),
+            Filter('year', whereIn: year)));
         debugPrint(projectTypology.toString());
         return projects.snapshots();
       }
       if (projectTypology.isNotEmpty && status.isNotEmpty) {
         debugPrint('two');
         final db = FirebaseFirestore.instance;
-        final projects = db
-            .collection('projects')
-            .where('project typology', whereIn: projectTypology)
-            .where('status', whereIn: status);
+        final projects = db.collection('projects').where(Filter.and(
+            Filter('project typology', whereIn: projectTypology),
+            Filter('status', whereIn: status)));
 
         return projects.snapshots();
       }
       if (projectTypology.isNotEmpty && year.isNotEmpty) {
         debugPrint('three');
         final db = FirebaseFirestore.instance;
-        final projects = db
-            .collection('projects')
-            .where('project typology', whereIn: projectTypology)
-            .where('year', whereIn: year);
+        final projects = db.collection('projects').where(Filter.and(
+            Filter('project typology', whereIn: projectTypology),
+            Filter('year', whereIn: year)));
 
         return projects.snapshots();
       }
       if (status.isNotEmpty && year.isNotEmpty) {
         debugPrint('four');
         final db = FirebaseFirestore.instance;
-        final projects =
-            db.collection('projects').where('status', whereIn: status);
-        //  final second = db.collection('projects').where('year', whereIn: year);
-
+        final projects = db.collection('projects').where(Filter.and(
+            Filter('status', whereIn: status), Filter('year', whereIn: year)));
         return projects.snapshots();
       }
       if (projectTypology.isNotEmpty) {
@@ -118,91 +113,99 @@ class _HomeState extends State<Home> {
               }
               var docs = snapshot.data!.docs;
 
-              return GridView(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1, childAspectRatio: 1.75),
-                  children: docs
-                      .map((doc) => GridTile(
-                            header: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Stack(children: [
-                                const Text(
-                                  'ezikeaf',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 30),
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: GridView(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1, childAspectRatio: 1.75),
+                    children: docs
+                        .map((doc) => GridTile(
+                              header: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Stack(children: [
+                                  const Text(
+                                    'ezikeaf',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 30),
+                                  ),
+                                  Positioned(
+                                      right: 10,
+                                      child: IconButton(
+                                          icon: const Icon(Icons.menu),
+                                          color: Colors.white,
+                                          onPressed: () async {
+                                            var value =
+                                                await showTopModalSheet<Map?>(
+                                                    context, const TopModal());
+                                            debugPrint(value.toString());
+                                            if (value != null) {
+                                              setState(() {
+                                                projectTypology =
+                                                    value['project typology'];
+                                                status = value['status'];
+                                                year = value['year'];
+                                                isFiltered = true;
+                                              });
+                                            }
+                                          }))
+                                ]),
+                              ),
+                              child: Stack(fit: StackFit.expand, children: [
+                                Positioned(
+                                  child: CarouselSlider.builder(
+                                    options: CarouselOptions(
+                                      aspectRatio: 1,
+                                      viewportFraction: 1,
+                                      enableInfiniteScroll: true,
+                                    ),
+                                    itemCount: doc['photoLink'].length,
+                                    itemBuilder:
+                                        (context, itemIndex, realIndex) {
+                                      return Container(
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: NetworkImage(
+                                                  doc['photoLink'][itemIndex],
+                                                ))),
+                                      );
+                                    },
+                                  ),
                                 ),
                                 Positioned(
-                                    right: 10,
-                                    child: IconButton(
-                                        icon: const Icon(Icons.menu),
-                                        color: Colors.white,
-                                        onPressed: () async {
-                                          var value =
-                                              await showTopModalSheet<Map?>(
-                                                  context, const TopModal());
-                                          debugPrint(value.toString());
-                                          if (value != null) {
-                                            setState(() {
-                                              projectTypology =
-                                                  value['project typology'];
-                                              status = value['status'];
-                                              year = value['year'];
-                                              isFiltered = true;
-                                            });
-                                          }
-                                        }))
+                                    top: 200,
+                                    bottom: 200,
+                                    right: 200,
+                                    left: 200,
+                                    child: Center(
+                                        child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(doc['title'],
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                                fontSize: 30)),
+                                        Text(doc['location'],
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 18)),
+                                        OutlinedButton(
+                                            onPressed: () {},
+                                            child: const Text('Learn More'))
+                                      ],
+                                    )))
                               ]),
-                            ),
-                            child: Stack(fit: StackFit.expand, children: [
-                              Positioned(
-                                child: CarouselSlider.builder(
-                                  options: CarouselOptions(
-                                    aspectRatio: 1,
-                                    viewportFraction: 1,
-                                    enableInfiniteScroll: true,
-                                  ),
-                                  itemCount: doc['photoLink'].length,
-                                  itemBuilder: (context, itemIndex, realIndex) {
-                                    return Container(
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              fit: BoxFit.cover,
-                                              image: NetworkImage(
-                                                doc['photoLink'][itemIndex],
-                                              ))),
-                                    );
-                                  },
-                                ),
-                              ),
-                              Positioned(
-                                  top: 200,
-                                  bottom: 200,
-                                  right: 200,
-                                  left: 200,
-                                  child: Center(
-                                      child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(doc['title'],
-                                          style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 24)),
-                                      Text(doc['location'],
-                                          style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 18)),
-                                      OutlinedButton(
-                                          onPressed: () {},
-                                          child: const Text('Learn More'))
-                                    ],
-                                  )))
-                            ]),
-                          ))
-                      .toList());
+                            ))
+                        .toList()),
+              );
             }));
   }
 }
